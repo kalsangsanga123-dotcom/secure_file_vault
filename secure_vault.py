28e7d4db3b102adbe17ca49a4accc26860c5356a
kalsangsanga123-dotcom <kalsangsanga123@gmail.com>
2026-02-15 21:56:30 +0545

    encryotion

--- secure_vault.py
+++ secure_vault.py
@@ -66,4 +66,5 @@ def encrypt_file(filepath: str, password: str):
 
     # Encrypt with unique salt per file
     salt = os.urandom(16)
+    encrypted = Fernet(derive_key(password, salt)).encrypt(data)
           
