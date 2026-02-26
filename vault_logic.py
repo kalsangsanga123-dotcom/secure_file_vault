36bdd5fc8475d6e6703ca260589d169852fe0e54
kalsangsanga123-dotcom <kalsangsanga123@gmail.com>
2026-02-26 22:56:45 +0545

    implement modular secure vault with PBKDF2 key derivation

--- vault_logic.py
+++ vault_logic.py
@@ -5,15 +5,15 @@ from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 
-    vault_path = os.path.join(VAULT_DIR, meta[filename]["vault_path"])
-    with open(vault_path, 'rb') as f:
-        salt = f.read(16)
-        encrypted = f.read()
-    
-    try:
-        data = Fernet(derive_key(password, salt)).decrypt(encrypted)
-    except InvalidToken:
-        raise ValueError("Wrong password or corrupted file!")
-    
-    with open(output_path, 'wb') as f:
-        f.write(data)
+VAULT_DIR = ".secure_vault"
+METADATA_FILE = os.path.join(VAULT_DIR, "vault.dat")
+VERIFY_FILE = os.path.join(VAULT_DIR, ".verify")
+os.makedirs(VAULT_DIR, exist_ok=True)
+
+def derive_key(password: str, salt: bytes) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000
+    )
